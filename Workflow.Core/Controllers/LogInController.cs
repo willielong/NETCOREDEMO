95cffe78cadf52834f2df0fae8b7940b4f2f2550
645544470 <645544470@qq.com>
2018-09-19 22:31:05 +0800

    添加jwt登录认证

---
+++ Workflow.Core/Controllers/LogInController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Workflow.comm;
+
+namespace Workflow.Core.Controllers
+{
+    [Route("api/LogIn")]
+    public class LogInController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody]UserModel user)
+        {
+            var tpm = new TokenBusiness();
+            var token = tpm.GenerateToken(HttpContext, user.userName, "").Result;
+            if (null != token)
+            {
+                return new JsonResult(token);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+    public class UserModel
+    {
+        public string userName { get; set; }
+        public string pwd { get; set; }
+    }
+}
--- Workflow.comm/AccessToken.cs
+++ Workflow.comm/AccessToken.cs
@@ -10,10 +10,14 @@
 * **************************************************************************
 */
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Workflow.comm
 {
@@ -60,5 +64,78 @@ namespace Workflow.comm
         /// 签名证书
         /// </summary>
         public SigningCredentials SigningCredentials { get; set; }
+    }
+
+    /// <summary>
+    /// Token提供类
+    /// </summary>
+    public class TokenBusiness
+    {
+        public TokenBusiness()
+        {
+        }
+        /// <summary>
+        /// 生成令牌
+        /// </summary>
+        /// <param name="context">http上下文</param>
+        /// <param name="username">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="role">角色</param>
+        /// <returns></returns>
+        public async Task<AccessToken> GenerateToken(HttpContext context, string username, string role)
+        {
+            var identity = await GetIdentity(username);
+            if (identity == null)
+            {
+                return null;
+            }
+            var now = DateTime.UtcNow;
+            //声明
+            var claims = new Claim[]
+            {
+             new Claim(JwtRegisteredClaimNames.Sub,username),
+             new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+             new Claim(JwtRegisteredClaimNames.Iat,ToUnixEpochDate(now).ToString(),ClaimValueTypes.Integer64),
+             new Claim(ClaimTypes.Role,role),
+             new Claim(ClaimTypes.Name,username)
+            };
+
+            var skey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(ServiceLocator.tokenHelper.TokenSecreKey));
+            var signingCredentials = new SigningCredentials(skey, SecurityAlgorithms.HmacSha256);
+            //Jwt安全令牌
+            var jwt = new JwtSecurityToken(
+                issuer: ServiceLocator.tokenHelper.Issuer,
+                audience:ServiceLocator.tokenHelper.Audience,
+                claims: claims,
+                notBefore: now,
+                expires: now.Add(TimeSpan.FromSeconds(double.Parse(ServiceLocator.tokenHelper.Expiration))),
+                signingCredentials: signingCredentials);
+            //生成令牌字符串
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            var response = new AccessToken
+            {
+                access_token = encodedJwt,
+                expires_in = (TimeSpan.FromSeconds(double.Parse(ServiceLocator.tokenHelper.Expiration))).Seconds
+            };
+            return response;
+        }
+
+        private static long ToUnixEpochDate(DateTime date)
+        {
+            return (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+        }
+        /// <summary>
+        /// 查看令牌是否存在
+        /// </summary>
+        /// <param name="username">用户名</param>
+        /// <returns></returns>
+        private Task<ClaimsIdentity> GetIdentity(string username)
+        {
+            return Task.FromResult(
+                new ClaimsIdentity(new System.Security.Principal.GenericIdentity(username, "token"),
+                new Claim[] {
+                 new Claim(ClaimTypes.Name, username)
+                }));
+        }
     }
 }
